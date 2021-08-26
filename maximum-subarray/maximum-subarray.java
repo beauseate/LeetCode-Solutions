0c522f71eeb28d4a53d752cbcf6604fcf2bacf16
Beau Seate <rseate@uncc.edu>
2021-08-26 13:51:12 -0400

    Time: 2 ms (13.98%), Space: 41.6 MB (5.59%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -1,13 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sumSoFar = nums[0];
-        int maxSum = nums[0];
+        
+        int currMax = nums[0];
+        int overallMax = nums[0];
         
         for(int i = 1; i < nums.length; i++){
-            sumSoFar = Math.max(sumSoFar + nums[i], nums[i]);
-            maxSum = Math.max(maxSum, sumSoFar);
+            
+            currMax = Math.max(currMax + nums[i], nums[i]);
+            overallMax = Math.max(overallMax, currMax);
         }
         
-        return maxSum;
+        return overallMax;
+        
     }
 }
