06948e99a5c7082d184eb068c2f11d83ef478618
Beau Seate <rseate@uncc.edu>
2021-09-14 08:44:09 -0400

    Time: 0 ms (100.00%), Space: 39 MB (21.84%) - LeetHub

---
+++ remove-element/remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int k = 0;
+        for(int i: nums){
+            if(i != val){
+            nums[k] = i;
+            k += 1;
+            }
+        }
+        return k;
+        
+    }
+}
