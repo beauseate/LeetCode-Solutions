69ba9c6baa720f8a55488a50c47b7dae66752e60
Beau Seate <rseate@uncc.edu>
2021-10-13 16:33:13 -0400

    Time: 1847 ms (26.48%), Space: 52.6 MB (50.69%)

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+        intervals.sort()
+        first = 0
+        last = 1
+        result = 0
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                result += 1
+                prevEnd = min(prevEnd, end)
+        return result
+        
