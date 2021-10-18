fbf1f4ba0338c8285f0b9a032806d7463c4889ab
Beau Seate <rseate@uncc.edu>
2021-10-18 17:54:08 -0400

    Time: 44 ms (61.51%), Space: 16.1 MB (41.47%)

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -10,5 +10,5 @@ class Solution:
         if not root:
             return 0
         
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
         
