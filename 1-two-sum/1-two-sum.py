b922dbc9ac9508ea3f223c0beb50a28e53ca7c06
Beau Seate <rseate@uncc.edu>
2021-10-18 14:10:05 -0400

    Time: 68 ms (62.94%), Space: 15.4 MB (41.66%)

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        d = {}
         
-        d = dict()
-        
-        for i, value in enumerate(nums):
-            difference = target - value
+        for i, num in enumerate(nums):
+            difference = target - num
+            
             if difference in d:
                 return {i, d.get(difference)}
-            d[value] = i
+            d[num] = i
         return {0, 0}
         
