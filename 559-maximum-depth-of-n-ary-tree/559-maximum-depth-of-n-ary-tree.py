94f91ba8c64af3f426341187cf80838892cb408e
Beau Seate <rseate@uncc.edu>
2021-10-18 23:05:55 -0400

    Time: 55 ms (33.40%), Space: 16.1 MB (47.90%)

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        
+        
+        if not root:
+            return 0
+        elif root.children == []:
+            return 1
+        else:
+            height = [self.maxDepth(c) for c in root.children]
+            return max(height) + 1
+        
