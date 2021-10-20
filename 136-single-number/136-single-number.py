d50b4b9053056d061f87ef5bad5184dbeeb9b20c
Beau Seate <rseate@uncc.edu>
2021-10-20 11:35:15 -0400

    Time: 214 ms (26.74%), Space: 16.7 MB (19.22%)

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+        for i in nums:
+            if i not in seen:
+                seen.add(i)
+            else:
+                seen.remove(i)
+        return min(seen)
+        
