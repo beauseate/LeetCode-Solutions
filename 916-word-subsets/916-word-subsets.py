613fe082779bbe39b98501028db5c41a7f6ccaaa
Beau Seate <rseate@uncc.edu>
2022-03-01 10:29:47 -0500

    Time: 1326 ms (39.46%), Space: 18.3 MB (69.92%)

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        
+        cnt = Counter()
+        for word in words2:
+            cnt |= Counter(word)
+            
+        return [a for a in words1 if not cnt - Counter(a)]
+        
